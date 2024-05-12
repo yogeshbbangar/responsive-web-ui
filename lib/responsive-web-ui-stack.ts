ba54ac82b1b5ac2c81764d6339b2196f98eb8de3
Yogesh Bangar <yogeshbbangar2@gmail.com>
2024-05-12 11:30:44 +0530

    CDK base code setup

---
+++ lib/responsive-web-ui-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class ResponsiveWebUiStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'ResponsiveWebUiQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
---
+++ test/responsive-web-ui.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ResponsiveWebUi from '../lib/responsive-web-ui-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/responsive-web-ui-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ResponsiveWebUi.ResponsiveWebUiStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
